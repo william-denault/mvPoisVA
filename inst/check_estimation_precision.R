eee87292271391d255863b49a99173ead88ae182
william-denault <william.denault@gmail.com>
2024-01-19 05:00:04 +0100

    Update check_estimation_precision.R

--- inst/check_estimation_precision.R
+++ inst/check_estimation_precision.R
@@ -31,7 +31,7 @@ X[,true_pos] <- X[,true_pos]-min( X[,true_pos] )
 noise_data <- list()
 for ( i in 1:N){
 
-  predictor <-   0.1*(DJ.EX(n=2^7)[[2]]) *X[i,true_pos] +rnorm(length(lf[[1]]), sd=.5)
+  predictor <-   0.1*(DJ.EX(n=2^7)[[4]]) *X[i,true_pos] +rnorm(length(lf[[1]]), sd=.5)
 
   noise_data[[i]]<- rpois(n, lambda=exp (predictor) )
 }
@@ -43,9 +43,10 @@ out$susiF.obj$cs
 true_pos
 out2$cs
 
-plot(  out$susiF.obj$fitted_func[[1]])
 
-lines( 0.1*(DJ.EX(n=2^7)[[2]]))
+plot( 0.051*(DJ.EX(n=2^7)[[4]]))
 lines (out2$fitted_func[[1]])
+lines(  2*out$susiF.obj$fitted_func[[1]],col="green" )
+
 lines(mu)
 plot ( out$susiF.obj$lfsr_func[[1]])
